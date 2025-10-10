215cec5a3a1f7d1fbfd346176566f45d04eb55b3
Lis <natcarriollo@gmail.com>
2025-10-10 09:46:10 -0500

    Comentarios de app.py
    Se agregaron algunos comentarios de futuras mejoras y explicación del código de este módulo.

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ def registrar_cedula(cedula):
     # Buscar la cédula en la base de datos
     persona = cedulas.find_one({"cedula": cedula})
 
-    if not persona:
-        print(f"La cédula {cedula} no está en la base de datos.")
+    if not persona: #En dado caso que la cédula no sea válida muestra un mensaje de error.
+        print(f"La cédula {cedula} no está en la base de datos.") 
         return
-
-    if persona["sexo"] == "Femenino":
-        print(f"{persona['nombre']} {persona['apellido1']} registrada exitosamente como usuaria.")
-    else:
-        print(f"La cédula {cedula} corresponde a un hombre ({persona['nombre']} {persona['apellido1']}). Registro denegado.")
+        
+    #Podría agregar un .lower para evitar errores
+    if persona["sexo"] == "Femenino": #Comprobación de una cédula femenina
+        print(f"{persona['nombre']} {persona['apellido1']} registrada exitosamente como usuaria, bienvenida.")  
+    else: #En dado caso que se ingrese una cédula másculina
+        print(f"La cédula {cedula} corresponde a un hombre ({persona['nombre']} {persona['apellido1']}). Registro denegado.") 
+        #Quitar nombre y apellido del hombre que trató de ingresar, por privacidad.
 
 # --- Ejemplo de uso ---
 if __name__ == "__main__":
