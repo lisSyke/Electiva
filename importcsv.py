5698a6dcd93ab3247a42c4b34c8a4e7e90a12a96
Lis <natcarriollo@gmail.com>
2025-10-10 10:05:24 -0500

    Comentarios importcsv.py
    Comentarios de cómo funciona el importe

--- importcsv.py
+++ importcsv.py
@@ -1,15 +1,17 @@
 import csv
 from db import cedulas   # conexión y colección desde db.py
 
-CSV_FILE = "sim_cedulas_femeninas.csv"
+CSV_FILE = "sim_cedulas_femeninas.csv" #Variable que se refiere a al documento csv con las cédulas
 
-def importar_csv():
-    with open(CSV_FILE, newline='', encoding='utf-8-sig') as f:
-        reader = csv.DictReader(f)
-        documentos = []
+def importar_csv(): #Función para importar el csv
+    with open(CSV_FILE, newline='', encoding='utf-8-sig') as f: 
+        # 'newline=""' evita errores con saltos de línea
+        # 'encoding="utf-8-sig"' asegura leer bien acentos, eñes y elimina el BOM
+        reader = csv.DictReader(f)  # Crea un lector de CSV que devuelve cada fila como un diccionario
+        documentos = []  # Lista vacía donde se almacenarán los documentos (filas) a insertar en la base de datos
 
-        for row in reader:
-            documentos.append({
+        for row in reader:  # Recorre cada fila del archivo CSV
+            documentos.append({ # Toma valores específicos de las columnas del CSV
                 "cedula": row["cedula_simulada"],
                 "nombre": row["nombre"],
                 "apellido1": row["apellido1"],
@@ -19,11 +21,12 @@ def importar_csv():
                 "ciudad": row["ciudad"],
                 "correo": row["correo_simulado"]
             })
-
-        if documentos:
+            
+        # Si la lista 'documentos' no está vacía (es decir, si se leyó algo)
+        if documentos:  # Inserta todos los documentos en la colección 'cedulas' de MongoDB Atlas
             cedulas.insert_many(documentos)
-            print(f"✅ Se insertaron {len(documentos)} registros en MongoDB Atlas.")
+            print(f"Se insertaron {len(documentos)} registros en MongoDB Atlas.")
 
 if __name__ == "__main__":
-    importar_csv()
+    importar_csv() 
 
